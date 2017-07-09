7ccc33c14f9a5b4f1b60f93050cf34819d75e834
wk-j <somnuk.wk@gmail.com>
2017-07-10 01:54:44 +0700

    C#

--- P11-20/P15/FSharp.fsx
+++ P11-20/P15/FSharp.fsx
@@ -2,6 +2,11 @@ let dup count xs = xs |> List.map (List.replicate count) |> List.collect id
 
 [1;2;3] |> dup 3 |> printfn "%A"
 
+[ "A"
+  "B"
+  "C" ] |> dup 3 |> printfn "%A"
+
+
 
 let dup1 (x: int) = [x; x]
 let dup2 x = [x; x]  : int list
