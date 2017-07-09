95d302ad43a4941a4b4e591bc0283da2d7ed792e
wk-j <somnuk.wk@gmail.com>
2017-07-10 02:31:12 +0700

    Problem 16

---
+++ P11-20/P16/FSharp.fsx
@@ -0,0 +1,16 @@
+
+let rec drop n xs = 
+    let rec find xs = 
+        let h = xs |> List.truncate (n-1)
+        if xs.Length > (n-1) then
+            let tail = xs |> List.skip (n)
+            h @ find tail
+        else
+            h
+    find xs
+
+['a' .. 'k']    |> drop 3 |> printfn "%A"
+[1 .. 10]       |> drop 3 |> printfn "%A"      
+
+
+
