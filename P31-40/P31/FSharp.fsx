2b672bb7e605a34bc004e75c63998103eeb9781f
wk-j <somnuk.wk@gmail.com>
2017-07-20 02:14:24 +0700

    Update

--- P31-40/P31/FSharp.fsx
+++ P31-40/P31/FSharp.fsx
@@ -1,19 +1,11 @@
+open System
 
 let isPrime x = 
     if x % 2 = 0 && x <> 2 then
         false
     else
-        [3 .. 2 .. x - 1]
-        |> List.map (fun k -> x % k = 0) 
-        |> List.forall (not)
+        [3 .. 2 .. int <| Math.Floor(Math.Sqrt(float x))]
+        |> List.takeWhile (fun k -> x % k = 0) 
+        |> List.isEmpty
 
-7   |> isPrime |> printfn "%A" 
-5   |> isPrime |> printfn "%A"
-11  |> isPrime |> printfn "%A"
-13  |> isPrime |> printfn "%A"
-17  |> isPrime |> printfn "%A"
-5   |> isPrime |> printfn "%A"
-
-2  |> isPrime |> printfn "%A"
-8  |> isPrime |> printfn "%A"
-15 |> isPrime |> printfn "%A"
+100000002 |> isPrime |> printfn "%A"
