ec3cb7ab9c9666cafa1adbb2b302b0e4930eb168
wk-j <somnuk.wk@gmail.com>
2017-08-05 20:11:32 +0700

    Problem 12 with Elm

--- P11-20-(List)/P12/Elm.elm
+++ P11-20-(List)/P12/Elm.elm
@@ -1,12 +1,27 @@
 import Html exposing (text)
 import List
 
-range : Int -> Int -> List Int
-range n1 n2 = 
-    if n1 <= n2  then
-        n1 :: range (n1 + 1) n2
-    else []
+type Item a
+    = Single a
+    | Multiple Int a
+
+decodeMofified : List (Item a) -> List a
+decodeMofified list = 
+    case list of
+        [] -> []
+        h :: xs ->
+            case h of
+                Single a -> 
+                    a :: decodeMofified xs
+                Multiple n a -> 
+                    if n > 1 then
+                        a :: decodeMofified (Multiple (n - 1) a :: xs)
+                    else 
+                        a :: decodeMofified xs
 
 main : Html.Html msg
-main = 
-    range 4 9 |> toString |> text
+main =
+    [Multiple 3 'a', Single 'b', Multiple 3 'c', Single 'd', Multiple 5 'e']
+    |> decodeMofified
+    |> toString 
+    |> text
